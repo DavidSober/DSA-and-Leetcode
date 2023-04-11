e592634e8c4a3730f8f8137ffd3d0ec10c601a44
DavidSober <ocon2010@yahoo.com>
2023-04-10 22:28:01 -0700

    Update 2500.py

--- LeetcodeProblems/2500.py
+++ LeetcodeProblems/2500.py
@@ -2,12 +2,9 @@
 
 class Solution:
     def deleteGreatestValue(self, grid: List[List[int]]) -> int:
-
         ans = 0
-
         for i in range(len(grid)): # in place sorting grid rows
             grid[i] = sorted(grid[i], reverse=True)
-        #print(grid)
         for i in range(len(grid[0])):
             col = [row[i] for row in grid]
             ans += max(col)

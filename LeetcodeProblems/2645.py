766202bb9dd125a989546ef42e0fba6ec562c598
DavidSober <ocon2010@yahoo.com>
2023-04-17 13:57:33 -0700

    Update 2645.py

--- LeetcodeProblems/2645.py
+++ LeetcodeProblems/2645.py
@@ -9,11 +9,20 @@
 # letters we need to add to get the end result. That difference is the answer.
 
 def addMinimum(word):
-    k, prev = 0, 'z'
-    for c in word:
-        k += c <= prev
-        prev = c
-    return k * 3 - len(word)
+    # goal is to find how many instances of abc should exist then subtrack the input len from that to get the num of 
+    # chars we should add
+    # we initialize prev to z since z will always be greater than the first char in word and thus we will always have a 
+    # min of k = 1 which is needed since the input will always have at LEAST one abc. since inputs are not empty
+    # thus k must be at least 1 and prev = z accomplishes that
+    k = 0
+    prev = 'z'
+
+    for curr in word:
+        k += curr <= prev # we incriment k if the prev char is greater than the curr char bc that would break increasing order
+        prev = curr # make current char the prev for the next iteration
+    return k * 3 - len(word) # taking the difference between the len of abcs that will be there if we add 
+    # all a's b's and c's like the problem asks. Difference between that and input is num of chars needed 
+    # to get list of just abc's 
 
 word = "aaaabb"
 addMinimum(word)    

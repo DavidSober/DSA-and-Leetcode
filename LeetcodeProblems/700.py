c72b5f4db30d4b6944d59ba02460ec85ddf8855e
DavidSober <ocon2010@yahoo.com>
2023-04-18 00:41:36 -0700

    prob 700 added

---
+++ LeetcodeProblems/700.py
@@ -0,0 +1,23 @@
+# beats 66% in time and 13% in space
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        def dfs(node):
+            if not node:
+                return 
+            if node.val == val:
+                return node
+
+            if node.val > val:
+                return dfs(node.left)
+            if node.val < val:
+                return dfs(node.right)
+
+        return dfs(root)            

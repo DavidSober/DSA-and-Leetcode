6b9412adb5e496aaa815641e2b4d440c3ac7aedd
DavidSober <ocon2010@yahoo.com>
2023-04-22 21:48:27 -0700

    Create 2652.py

---
+++ LeetcodeProblems/2652.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        ans = []
+        for i in range(1, n + 1):
+            if i % 3 == 0:
+                ans.append(i)
+                continue
+            elif i % 5 == 0:
+                ans.append(i)
+                continue
+            elif i % 7 == 0:
+                ans.append(i)
+                continue
+        return sum(ans)

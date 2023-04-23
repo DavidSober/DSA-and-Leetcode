3e1a247c82a45c3ea173774d94b85630f58b7d12
DavidSober <ocon2010@yahoo.com>
2023-04-22 19:03:56 -0700

    Update 268.py

--- LeetcodeProblems/268.py
+++ LeetcodeProblems/268.py
@@ -6,4 +6,14 @@ class Solution:
         nums_set = set(nums)
         diff = pot_set ^ nums_set
         return list(diff)[0]
+    
+# my first one liner! uses bitwise difference
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return list(set(nums) ^ set([i for i in range(len(nums) + 1)]))[0]
+        
+# another one liner but much better stats: uses sum difference 
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return sum((i for i in range(0, len(nums) + 1))) - sum((i for i in nums))
         

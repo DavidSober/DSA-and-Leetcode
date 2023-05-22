62604eaa6a11b772dde7141f91c59382086addda
DavidSober <ocon2010@yahoo.com>
2023-05-22 00:17:49 -0700

    Update 347.py

--- LeetcodeProblems/347.py
+++ LeetcodeProblems/347.py
@@ -1,5 +1,16 @@
-# 78% in time and 30% in space
+# O(n * k) not sure that this is better
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        F = Counter(nums)
+        ans = []
+        for i in range(k):
+            mx = max(F, key = lambda x: F[x])
+            ans.append(mx)
+            F[mx] = 0
+        return ans 
 
+# 46% in time and 51% in space
+# time comp for this is O(N log N) can we do better? yes
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         F = Counter(nums)

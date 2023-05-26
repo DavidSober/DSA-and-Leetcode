497f9a35d2de297f583835d5355446f21ee14523
David Soberanis <ocon2010@yahoo.com>
2023-05-26 02:37:50 -0700

    Update 295.py

--- LeetcodeProblems/295.py
+++ LeetcodeProblems/295.py
@@ -32,3 +32,27 @@ class MedianFinder:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+
+# here is the sorted list version anyway
+# beats 20% in time and 10% in space
+from sortedcontainers import SortedList
+class MedianFinder:
+
+    def __init__(self):
+        self.sl = SortedList()
+
+    def addNum(self, num: int) -> None:
+        self.sl.add(num)
+
+    def findMedian(self) -> float:
+        n = len(self.sl)
+        if len(self.sl) % 2 == 0:
+            return (self.sl[n//2] + self.sl[ n//2 - 1]) / 2
+        else:
+            return self.sl[ n//2 ]
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()

0c12387178f4ffa720c856a3c4d94c942680f61d
DavidSober <ocon2010@yahoo.com>
2023-06-05 22:44:38 -0700

    Update 1502.py

--- LeetcodeProblems/1502.py
+++ LeetcodeProblems/1502.py
@@ -1,3 +1,15 @@
+# faster method avoids using a set just a simple delta comparison
+#beats 47% in time and 51% in space
+
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        delta = arr[1] - arr[0]
+        for i in range(len(arr) - 1):
+            if arr[i + 1] - arr[i] != delta:
+                return False
+        return True
+
 # beats 23% in time and 20% in space
 
 class Solution:

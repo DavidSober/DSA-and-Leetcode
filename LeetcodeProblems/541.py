356f494ab094f529a868f718eef1ea53c1b08885
DavidSober <ocon2010@yahoo.com>
2023-06-07 20:00:43 -0700

    Create 541.py

---
+++ LeetcodeProblems/541.py
@@ -0,0 +1,8 @@
+# beats 38% in time and 48% in space
+
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        lst = list(s)
+        for i in range(0, len(s), 2*k):
+            lst[i:i+k] = reversed(lst[i:i+k])
+        return "".join(lst)

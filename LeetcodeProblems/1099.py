b08d77733b18ce040d4b94eb45cf23e5b6a2cef8
DavidSober <ocon2010@yahoo.com>
2023-06-15 14:56:57 -0700

    Update 1099.py

--- LeetcodeProblems/1099.py
+++ LeetcodeProblems/1099.py
@@ -1,3 +1,21 @@
+# two pointers on sorted list approach
+# beats 54% in time and 26% in space
+
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        ans = -1
+        while left < right:
+            if nums[left] + nums[right] < k:
+                ans = max(ans, nums[left] + nums[right])
+                left += 1
+            else:
+                right -= 1
+        return ans
+
+# binary search approach
 # beats 23% in time and 64% in space
 
 class Solution:

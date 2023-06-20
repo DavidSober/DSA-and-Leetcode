d20ffc5e14d7d3d4d88e8e57a455e4206773e5d2
DavidSober <ocon2010@yahoo.com>
2023-06-19 20:34:27 -0700

    Update 1150.py

--- LeetcodeProblems/1150.py
+++ LeetcodeProblems/1150.py
@@ -1,3 +1,12 @@
+# used binary search for this one
+# beats 21% in time and 87% in space
+
+class Solution:
+    def isMajorityElement(self, nums: List[int], target: int) -> bool:
+        L = bisect.bisect_left(nums, target)
+        R = bisect.bisect_right(nums, target)
+        return R-L > len(nums) / 2
+
 # beats 86% in time and 22% in space
 
 class Solution:

0b4d0b7732697dda2dcd5c75efff1e519ed57985
David Soberanis <ocon2010@yahoo.com>
2023-06-21 11:09:14 -0700

    Create 2300.py

---
+++ LeetcodeProblems/2300.py
@@ -0,0 +1,17 @@
+# beats 72% in time and 16% in space
+
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        ans = []
+        n = len(potions)
+        potions.sort()
+        for spell in spells:
+            x = math.ceil(success / spell)
+            pot = bisect.bisect_left(potions, x)
+            print(pot)
+            if pot >= n:
+                ans.append(0)
+                continue
+            if potions[pot]*spell >= success:
+                ans.append(n - pot)
+        return ans

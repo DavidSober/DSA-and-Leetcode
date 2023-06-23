b84cec30062209fee04721c9f163a1dde06c405d
David Soberanis <ocon2010@yahoo.com>
2023-06-23 15:20:58 -0700

    Update 1213.py

--- LeetcodeProblems/1213.py
+++ LeetcodeProblems/1213.py
@@ -1,3 +1,9 @@
+# same as below but in one liner format 
+
+class Solution:
+    def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
+        return sorted(list(set(arr1) & set(arr2) & set(arr3)))
+
 # used bitwise and to get the elements in common much faster than with hashmap
 # beats 94% in time and 20% in space
 

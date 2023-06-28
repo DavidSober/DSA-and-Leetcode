4645043abf6133f9014c3c3be55cdf7ede672ac0
DavidSober <ocon2010@yahoo.com>
2023-06-28 16:21:45 -0700

    Update 1064.py

--- LeetcodeProblems/1064.py
+++ LeetcodeProblems/1064.py
@@ -1,3 +1,19 @@
+# bin search solution is much better logn << n 
+# beats 92% in time and 46% in space
+class Solution:
+    def fixedPoint(self, arr: List[int]) -> int:
+        left = 0
+        right = len(arr) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] >= mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        if left > len(arr) - 1 or arr[left] != left:
+            return -1
+        return left 
+
 # beats 82% in time and 88% in space
 
 class Solution:

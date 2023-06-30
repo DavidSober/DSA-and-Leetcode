a9ccbb58fe923b9531930f0a50d0009cf4048a1c
DavidSober <ocon2010@yahoo.com>
2023-06-30 15:03:12 -0700

    Update 206.py

--- LeetcodeProblems/206.py
+++ LeetcodeProblems/206.py
@@ -1,3 +1,18 @@
+# although this method is not as clean as the first this one makes the most sense
+# if you draw it out 
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            NN = curr.next
+            curr.next = prev
+            prev = curr
+            curr = NN
+        return prev
+
+
 # this is the fastest and easiest way to reverse a linked list from what I have seen...
 # beats 66% in time and 78% in space
 

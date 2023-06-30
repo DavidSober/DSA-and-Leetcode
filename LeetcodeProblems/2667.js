4115add803527f64fa71c1570dd5803948560d1a
DavidSober <ocon2010@yahoo.com>
2023-06-29 23:56:11 -0700

    Create 2667.js

---
+++ LeetcodeProblems/2667.js
@@ -0,0 +1,16 @@
+// first js code on leetcode its been what 7 years since i last used this lang?
+// beats 93% in time and 27% in space
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return 'Hello World'
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */

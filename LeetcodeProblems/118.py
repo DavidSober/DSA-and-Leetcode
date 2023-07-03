7007b071d28ce46a95504802c2c4c31ced906027
DavidSober <ocon2010@yahoo.com>
2023-07-02 19:42:28 -0700

    Create 118.py

---
+++ LeetcodeProblems/118.py
@@ -0,0 +1,22 @@
+# beats 60% in time and 7% in space
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        cache = {}
+        def Pnum(row, col):
+            if row == 0 or col == 0 or row == col:
+                return 1
+            if (row, col) in cache:
+                return cache[(row, col)]
+            else:
+                result = Pnum(row - 1, col - 1) + Pnum(row - 1, col)
+            cache[(row, col)] = result
+            return result
+        ans = []
+        for row in range(numRows - 1, 0, -1):
+            temp = []
+            for col in range(row + 1):
+                temp.append(Pnum(row, col))
+            ans.append(temp)
+        ans.append([1])
+        return ans[::-1]

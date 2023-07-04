c3c8434f3a0f649fd0c28b09cfbf0e77571cdd70
DavidSober <ocon2010@yahoo.com>
2023-07-04 12:51:46 -0700

    Create 2235.py

---
+++ LeetcodeProblems/2235.py
@@ -0,0 +1,6 @@
+# after 280 problems I have finally found something easier than twosum
+# beats all logic and effort possible 
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2

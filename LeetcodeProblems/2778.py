3a770d0a0b7bacead56af8221e8805295b87e75e
DavidSober <ocon2010@yahoo.com>
2023-07-20 12:37:25 -0700

    Update 2778.py

--- LeetcodeProblems/2778.py
+++ LeetcodeProblems/2778.py
@@ -1,4 +1,10 @@
- # beats 50% in time and 93% in space
+# one liner 
+
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        return sum(nums[i]**2 for i in range(len(nums)) if len(nums) % (i + 1) == 0)
+    
+# beats 50% in time and 93% in space
 
 class Solution:
     def sumOfSquares(self, nums: List[int]) -> int:

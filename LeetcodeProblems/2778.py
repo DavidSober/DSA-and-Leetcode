4ce6d1317e2671b08eb9c96da9d0d7bca00badd3
DavidSober <ocon2010@yahoo.com>
2023-07-20 12:32:38 -0700

    Create 2778.py

---
+++ LeetcodeProblems/2778.py
@@ -0,0 +1,9 @@
+ # beats 50% in time and 93% in space
+
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(len(nums)):
+            if len(nums) % (i + 1) == 0:
+                ans += nums[i]**2
+        return ans

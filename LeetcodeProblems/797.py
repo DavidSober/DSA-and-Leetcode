3889ea6d5f11d872390fbf2dbd9d2c18d9bae853
DavidSober <ocon2010@yahoo.com>
2023-08-01 03:52:17 -0700

    Create 797.py

---
+++ LeetcodeProblems/797.py
@@ -0,0 +1,25 @@
+# beats 94% in time and 8% in space
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        ans = []
+        graphN = defaultdict(list)
+        for i in range(len(graph)):
+            for neighbor in graph[i]:
+                graphN[i].append(neighbor)
+        
+        seen = set()
+        def backtrack(curr, i):
+            if i == len(graph) - 1:
+                ans.append(curr[:])
+                return
+        
+            for neighbor in graph[i]:
+                if neighbor not in curr:
+                    print(neighbor)
+                    curr.append(neighbor)
+                    backtrack(curr, neighbor)
+                    curr.pop()
+            
+        backtrack([0], 0)
+        return ans

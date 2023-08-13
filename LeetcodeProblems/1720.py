15030d9dc31d25752e2d6822970e8e8a566c9b36
DavidSober <ocon2010@yahoo.com>
2023-08-13 01:59:15 -0700

    Update 1720.py

--- LeetcodeProblems/1720.py
+++ LeetcodeProblems/1720.py
@@ -1,4 +1,11 @@
 # beats 78% in time and 28% in space
+'''Explanation
+cur means the current decoded element.
+
+For each element we have
+A[i] = res[i] ^ res[i+1]
+A[i] ^ A[i] ^ res[i+1] = res[i] ^ res[i+1] ^ A[i] ^ res[i+1]
+res[i+1] = res[i] ^ A[i]'''
 
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:

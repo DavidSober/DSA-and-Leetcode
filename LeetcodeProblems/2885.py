fec275b4df99bff0e2cfb5075ef20c2c6d0878cd
DavidSober <ocon2010@yahoo.com>
2023-11-19 17:46:44 -0800

    Update 2885.py

--- LeetcodeProblems/2885.py
+++ LeetcodeProblems/2885.py
@@ -1,3 +1,16 @@
+# this is the scalable right way to do it 
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    return students.rename(
+        columns = {
+            'id': 'student_id', 'first':'first_name', 'last':'last_name', 'age':'age_in_years'
+            }
+        )
+
+# this is the knee jerk reaction way i did it first
+# works only if you need to relabel all columns at once. if you need to relabel
+# only a few among many. this will not work
 import pandas as pd
 
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:

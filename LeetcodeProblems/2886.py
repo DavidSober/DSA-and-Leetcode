424b423fe6907dff432c14ed9f5087d5b3576b29
DavidSober <ocon2010@yahoo.com>
2023-11-19 18:00:46 -0800

    Update 2886.py

--- LeetcodeProblems/2886.py
+++ LeetcodeProblems/2886.py
@@ -1,3 +1,11 @@
+# using the right built in method
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students = students.astype({'grade': int})
+    return students
+
+# using list comprehension
 import pandas as pd
 
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:

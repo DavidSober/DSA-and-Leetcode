7569594a37d65acbfa33d21ad2bb87aa8c36781f
DavidSober <ocon2010@yahoo.com>
2023-12-02 21:54:58 -0800

    Create 1.cpp

---
+++ LeetcodeProblems Cpp/1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < len(nums); i++) {
+            for (int j = 0; j < len(nums);  j++){
+                if i < j {
+                    if nums[i] + nums[j] == target {
+                        return vector<int> 
+                    }
+                }
+            }
+                
+        }
+            
+
+
+    }   
+};

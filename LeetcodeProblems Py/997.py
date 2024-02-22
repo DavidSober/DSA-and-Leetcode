942748910135065421604de97b710fd0fd27cb03
David Soberanis <97570470+DavidSober@users.noreply.github.com>
2024-02-21 16:44:01 -0800

    Update 997.py

--- LeetcodeProblems Py/997.py
+++ LeetcodeProblems Py/997.py
@@ -1,3 +1,22 @@
+# revisit after many months 
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        candidates = []
+        F_right = Counter()
+        F_left = Counter()
+        for pair in trust:
+            F_right[pair[1]] += 1
+        for pair in trust:
+            F_left[pair[0]] += 1
+        for i in range(1, n + 1):
+            if i not in F_left.keys():
+                candidates.append(i)
+        for candidate in candidates:
+            if F_right[candidate] == n - 1:
+                return candidate
+        return -1
+
+
 # beats 30% in time and 36% in space
 
 from collections import defaultdict
